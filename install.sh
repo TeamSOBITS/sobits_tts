0565b7dfba1b50a274fe01fde5198d5c982672f0
f22hakuti <f22hakuti@gmail.com>
2025-08-15 20:06:28 +0900

    Update

--- install.sh
+++ install.sh
@@ -8,12 +8,12 @@ sudo apt install pulseaudio pulseaudio-utils -y
 
 sudo apt install -y ros-${ROS_DISTRO}-vision-msgs
 
-# Install "sobits_msgs"
+# Install "sobits_interfaces"
 cd ~/colcon_ws/src/
-if [ ! -d "sobits_msgs" ]; then
-    git clone -b ${ROS_DISTRO}-devel https://github.com/TeamSOBITS/sobits_msgs.git
+if [ ! -d "sobits_interfaces" ]; then
+    git clone -b ${ROS_DISTRO}-devel https://github.com/TeamSOBITS/sobits_interfaces.git
 else
-    echo "sobits_msgs リポジトリはすでに存在します。スキップします。"
+    echo "sobits_interfaces リポジトリはすでに存在します。スキップします。"
 fi
 
 pip3 install soundfile
