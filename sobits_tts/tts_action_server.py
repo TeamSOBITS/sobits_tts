a2cf9c7111a0656c5c169c79a37861ff741a94c6
Rjochi <f22hakuti@gmail.com>
2025-10-23 19:20:00 +0900

    [UPD] To save directory of sound_files.

--- sobits_tts/tts_action_server.py
+++ sobits_tts/tts_action_server.py
@@ -27,8 +27,7 @@ class TTSActionServer(Node):
         self._mixer_initialized = False
 
         try:
-            self.save_dir = os.path.join(os.path.abspath(os.path.join(get_package_share_directory('sobits_tts'), '..', '..', '..', '..')),
-                                        'src', 'sobits_tts', 'soundfile')
+            self.save_dir = os.path.join(get_package_share_directory('sobits_tts'), 'soundfile')
             self.output_filename = 'output.wav'
             self.output_filepath = os.path.join(self.save_dir, self.output_filename)
 
