016c62b7b3655eba9b5f88e30d42efb63b811594
satokeeen <kenta.sato5044@gmail.com>
2026-03-05 22:23:02 +0900

    [FIX] Add --break-system-packages.

--- install/kokoro.sh
+++ install/kokoro.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "╔══╣ Install: Kokoro TTS (STARTING) ╠══╗"
 
+export PIP_BREAK_SYSTEM_PACKAGES=1
+
 sudo apt update -y
 
 sudo apt install -y espeak-ng
@@ -11,6 +13,8 @@ pip3 install -q 'misaki[en,ja]==0.9.4' --break-system-packages
 pip3 uninstall -y fugashi unidic unidic-lite --break-system-packages
 pip3 install -q fugashi unidic-lite --break-system-packages
 
-pip3 install transformers==4.37.2
+pip3 install transformers==4.37.2 --break-system-packages
+
+pip3 install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.8.0/en_core_web_sm-3.8.0-py3-none-any.whl --break-system-packages
 
 echo "╚══╣ Install: Kokoro TTS (FINISHED) ╠══╝"
