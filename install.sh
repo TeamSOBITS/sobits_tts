d45ea671688e9f2d86680e743c69a2538e2a1a4c
MrKeith99 <kvalentincardenas@hotmail.com>
2026-03-31 19:37:05 +0900

    [FIX] colcon ws is not default ws

--- install.sh
+++ install.sh
@@ -9,9 +9,12 @@ sudo apt install pulseaudio pulseaudio-utils -y
 sudo apt install -y ros-${ROS_DISTRO}-vision-msgs
 
 # Install "sobits_interfaces"
-cd ~/colcon_ws/src/
+cd ../
 if [ ! -d "sobits_interfaces" ]; then
     git clone -b ${ROS_DISTRO}-devel https://github.com/TeamSOBITS/sobits_interfaces.git
+    cd sobbits_interfaces/
+    bash install.sh
+    cd ..
 else
     echo "sobits_interfaces リポジトリはすでに存在します。スキップします。"
 fi
@@ -21,4 +24,3 @@ pip3 install soundfile --break-system-packages
 pip3 install pygame --break-system-packages
 
 echo "╚══╣ Install: SOBITS TTS (FINISHED) ╠══╝"
-
